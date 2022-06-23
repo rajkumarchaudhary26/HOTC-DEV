acb6fdbed3fd937f8712efd41871db848ed37091
Raj Chaudhary <rajkumarchaudhary26@gmail.com>
2022-06-23 16:52:03 +0545

    Minor fixes

--- general/admin.py
+++ general/admin.py
@@ -1,28 +1,8 @@
 from django.contrib import admin
 
-from .models import BoardMembers, OrganizationStructure, Contact, Testimonial
+from .models import Home
 
 
-@admin.register(BoardMembers)
-class BoardMembersAdmin(admin.ModelAdmin):
-    list_display = ('name', 'designation', 'created_at', 'updated_at',)
-
-
-@admin.register(OrganizationStructure)
-class OrganizationStructureAdmin(admin.ModelAdmin):
-    list_display = ('title', 'created_at', 'updated_at',)
-
-
-@admin.register(Contact)
-class ContactAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email',)
-
-
-@admin.register(Testimonial)
-class Testimonial(admin.ModelAdmin):
-    list_display = ('title', 'created_at', 'updated_at',)
-    date_hierarchy = 'created_at'
-    list_filter = ('created_at', 'updated_at',)
-    list_per_page = 10
-    ordering = ('-created_at',)
-    search_fields = ('title',)
+@admin.register(Home)
+class HomeAdmin(admin.ModelAdmin):
+    list_display = ('content', 'uploaded_at', 'updated_at',)
