e40f85cea6887d7c5eace4c96a754368eaae07e5
Raj Chaudhary <rajkumarchaudhary26@gmail.com>
2022-06-23 16:50:20 +0545

    moved BoardMember and OrganizationStructure to team app

--- general/models.py
+++ general/models.py
@@ -1,38 +1,6 @@
 from django.db import models
 
-from versatileimagefield.fields import VersatileImageField, PPOIField
 from ckeditor.fields import RichTextField
-
-
-class BoardMember(models.Model):
-    name = models.CharField(max_length=50)
-    image = VersatileImageField(
-        upload_to='images/board_members/', null=True, blank=True, ppoi_field='ppoi')
-    ppoi = PPOIField()
-    designation = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.name
-
-    def get_absolute_url(self):
-        return 'board-member/{}'.format(self.name)
-
-
-class OrganizationStructure(models.Model):
-    title = models.CharField(max_length=255)
-    image = VersatileImageField(
-        upload_to='images/organization_structure/', null=True, blank=True, ppoi_field='ppoi')
-    ppoi = PPOIField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.title
-
-    class Meta:
-        verbose_name_plural = 'Organization Structure'
 
 
 class Home(models.Model):
@@ -46,27 +14,3 @@ class Home(models.Model):
 
     class Meta:
         verbose_name_plural = 'Home'
-
-
-# TODO: move to contact app
-class Contact(models.Model):
-    name = models.CharField(max_length=255)
-    email = models.EmailField(max_length=255)
-    message = RichTextField()
-
-    def __str__(self):
-        return self.name
-
-
-# TODO: move to testimonial app
-class Testimonial(models.Model):
-    title = models.CharField(max_length=255, db_index=True)
-    image = VersatileImageField(
-        upload_to='images/testimonial/', null=True, blank=True)
-    description = RichTextField()
-    created_at = models.DateTimeField(
-        auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.title
