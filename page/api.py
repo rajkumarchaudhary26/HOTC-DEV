4a1490929d2396268b4bbd47c0a0ed9450b07a46
Raj Chaudhary <rajkumarchaudhary26@gmail.com>
2022-06-23 16:54:25 +0545

    moved HomeViewSet to General app

--- page/api.py
+++ page/api.py
@@ -1,14 +1,9 @@
 from rest_framework import viewsets, mixins
 
-from .models import Page, Home
-from .serializers import PageSerializer, HomeSerializer
+from .models import Page
+from .serializers import PageSerializer
 
 
 class PageViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     queryset = Page.objects.all()
     serializer_class = PageSerializer
-
-
-class HomeViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
-    queryset = Home.objects.all()
-    serializer_class = HomeSerializer
