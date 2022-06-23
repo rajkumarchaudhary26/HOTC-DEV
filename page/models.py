699b099ae7fd9c057766cab393354945b340b440
Raj Chaudhary <rajkumarchaudhary26@gmail.com>
2022-06-23 16:08:32 +0545

    moved model 'Home' to General app

--- page/models.py
+++ page/models.py
@@ -19,16 +19,3 @@ class Page(models.Model):
 
     def get_absolute_url(self):
         return 'about/{}'.format(self.slug)
-
-
-class Home(models.Model):
-    content = RichTextField()
-    uploaded_at = models.DateTimeField(
-        auto_now_add=True, null=True, blank=True)
-    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
-
-    def __str__(self):
-        return self.content[:20]
-
-    class Meta:
-        verbose_name_plural = 'Home'
