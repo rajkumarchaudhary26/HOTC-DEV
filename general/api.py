94213217c42cde2793a9a4a6e3f2f2a2d69ef8c0
Raj Chaudhary <rajkumarchaudhary26@gmail.com>
2022-06-23 16:52:41 +0545

    Moved BoardMemberViewSet and OrganizationStructureViewset to team app

--- general/api.py
+++ general/api.py
@@ -1,24 +1,9 @@
 from rest_framework import viewsets, mixins
 
-from .models import BoardMembers, OrganizationStructure, Contact, Testimonial
-from .serializers import BoardMembersSerializer, OrganizationStructureSerializer, ContactSerializer, TestimonialSerializer
+from .models import Home
+from .serializers import HomeSerializer
 
 
-class BoardMembersViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.RetrieveModelMixin):
-    queryset = BoardMembers.objects.all()
-    serializer_class = BoardMembersSerializer
-
-
-class OrganizationStructureViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.RetrieveModelMixin):
-    queryset = OrganizationStructure.objects.all()
-    serializer_class = OrganizationStructureSerializer
-
-
-class ContactViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.CreateModelMixin, mixins.UpdateModelMixin):
-    queryset = Contact.objects.all()
-    serializer_class = ContactSerializer
-
-
-class TestimonialViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
-    queryset = Testimonial.objects.all()
-    serializer_class = TestimonialSerializer
+class HomeViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+    queryset = Home.objects.all()
+    serializer_class = HomeSerializer
