0216b070b4d04d3ac2adb22ea2e72a9717f8ae68
Raj Chaudhary <rajkumarchaudhary26@gmail.com>
2022-06-23 16:51:01 +0545

    moved BoardMemberSerializer and OrganizationStructureSerializer to team app

--- general/serializers.py
+++ general/serializers.py
@@ -1,42 +1,12 @@
 from rest_framework import serializers
 
-from .models import BoardMembers, OrganizationStructure, Contact, Testimonial
+from .models import Home
+from gallery.serializers import GallerySerializer
 
-from versatileimagefield.serializers import VersatileImageFieldSerializer
 
-
-class BoardMembersSerializer(serializers.ModelSerializer):
-    image = VersatileImageFieldSerializer(sizes=[
-        ('square', 'crop__200x200'),
-    ])
+class HomeSerializer(serializers.ModelSerializer):
+    gallery = GallerySerializer(many=True)
 
     class Meta:
-        model = BoardMembers
-        fields = ('id', 'name', 'image', 'designation',)
-
-
-class OrganizationStructureSerializer(serializers.ModelSerializer):
-    image = VersatileImageFieldSerializer(sizes=[
-        ('detail', 'thumbnail__1291x967'),
-    ])
-
-    class Meta:
-        model = OrganizationStructure
-        fields = ('id', 'title', 'image',)
-
-
-class ContactSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Contact
-        fields = ('id', 'name', 'email', 'message',)
-
-
-class TestimonialSerializer(serializers.ModelSerializer):
-    image = VersatileImageFieldSerializer(sizes=[
-        ('detail', 'thumbnail__1291x967'),
-    ])
-
-    class Meta:
-        model = Testimonial
-        fields = ('id', 'title', 'image', 'description',
-                  'created_at', 'updated_at',)
+        model = Home
+        fields = ('gallery', 'content', 'uploaded_at', 'updated_at',)
